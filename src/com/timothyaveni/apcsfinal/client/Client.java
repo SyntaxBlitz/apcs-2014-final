4a39edbcef7e95eb974a6d44a85d881cae5a8527
fisherd <danjsurf@gmail.com>
2014-05-20 09:32:31 -0400

    HI

--- src/com/timothyaveni/apcsfinal/client/Client.java
+++ src/com/timothyaveni/apcsfinal/client/Client.java
@@ -1,18 +1,31 @@
 package com.timothyaveni.apcsfinal.client;
 
 public class Client {
+	private final double FPS = 30.0;
 	/**render environment 
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		
+		//display GUI
+		gameLoop(true);		
 	}
 	
-	public void gameLoop () {
-		boolean isRunning;
+	public static void gameLoop (boolean isRunning) {
+		long lastLoopTime = System.nanoTime();
 		
+		while(isRunning){
+			lastLoopTime = System.nanoTime();
+			
+			//get player input
+			//move sprites
+			//render environment
+			
+			try{Thread.sleep((long)(1000000000 / FPS - (System.nanoTime()-lastLoopTime)));
+			
+			} catch(InterruptedException e){}
+			
+		}
 	}
 	
 }
