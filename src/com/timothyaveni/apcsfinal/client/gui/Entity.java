3ad9acfa48095c04ba3b180eeba70ed65e742fc1
CMM1313 <c.m.muller@hotmail.com>
2014-05-20 09:16:19 -0400

    Entity update

---
+++ src/com/timothyaveni/apcsfinal/client/gui/Entity.java
@@ -0,0 +1,25 @@
+package com.timothyaveni.apcsfinal.client.gui;
+
+public abstract class Entity {
+
+	public int height;
+	public int width;
+	public String name;
+	public String fileLocation;
+	
+	public Entity (String name, String location, int height, int width){
+		this.name = name;
+		fileLocation = location;
+		this.height = height;
+		this.width = width;
+	}
+	public String getName(){
+		return name;
+	}
+	public int getHeight(){
+		return height;
+	}
+	public int getWidth(){
+		return width;
+	}
+}
