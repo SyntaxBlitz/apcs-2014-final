bbee47b626431f3232d2d979fef119f0a141b829
Timothy Aveni <timothy@tacosareawesome.com>
2014-05-25 15:30:01 -0400

    Add ByteArrayTools and JUnit test
    These methods are useful for packing integers into byte arrays and then
    unpacking them later. I've decided on big-endian, which I mentioned in
    packetStructure.txt, because it's more readable when you're looking at
    the raw byte data. It makes the code a little more complicated, but
    ideally I shouldn't have to look at this again. If I do, I expect it'll
    be because of some problem with integers being signed or something.

---
+++ src/com/timothyaveni/apcsfinal/networking/ByteArrayTools.java
@@ -0,0 +1,73 @@
+package com.timothyaveni.apcsfinal.networking;
+
+import sun.awt.windows.ThemeReader;
+import sun.security.util.Length;
+
+/**
+ * This is a class that works in big-endian to place integers into byte arrays
+ * and read integers from byte arrays. This is used in place of ByteBuffer
+ * because it allows for more flexibility in int size and will also shove values
+ * straight into the array.
+ * 
+ * @author Timothy Aveni
+ * 
+ */
+public class ByteArrayTools {
+
+	/**
+	 * Returns an int that's derived from the corresponding section of the byte
+	 * array. Reads bytes in big-endian.
+	 * 
+	 * @param data
+	 *            the byte array to analyze
+	 * @param startIndex
+	 *            the first index to consider in the byte array
+	 * @param length
+	 *            the length, in bytes, of the integer to analyze. MUST BE in
+	 *            the range 1..4.
+	 * @return the computed integer, or -1 if the length is > 4 or < 1 or
+	 *         (startIndex + length) > data.length
+	 */
+	public static int readBytes(byte[] data, int startIndex, int length) {
+		if (length < 1 || length > 4 || startIndex + length > data.length)
+			return -1;
+
+		int toReturn = 0;
+		int pow = 0; // don't strictly need this variable but it's nice to have
+		for (int i = startIndex + length - 1; i >= startIndex; i--) {
+			toReturn += (data[i] & 0xFF) * (Math.pow(256, pow));
+			pow++;
+		}
+		return toReturn;
+	}
+
+	/**
+	 * Modifies the referenced array to include an integer at the specified
+	 * location with the appropriate number of bytes. Will truncate an integer
+	 * to its least-significant bytes if it is larger than the specified length.
+	 * Sets bytes in big-endian.
+	 * 
+	 * @param data
+	 *            the byte array to modify
+	 * @param value
+	 *            the integral value to place into the byte array
+	 * @param startIndex
+	 *            the first index in the byte array that should be overridden
+	 * @param length
+	 *            the number of bytes to use to store this number. MUST BE in
+	 *            the range 1..4.
+	 */
+	public static void setBytes(byte[] data, int value, int startIndex, int length) {
+		if (length < 1 || length > 4 || startIndex + length > data.length)
+			return;
+
+		int pow = length - 1;
+		for (int i = startIndex; i < startIndex + length; i++) {
+			int thisByte = (int) (value / Math.pow(256, pow));
+			data[i] = (byte)(thisByte & 0xFF);
+			value -= thisByte * Math.pow(256, pow);
+			pow--;
+		}
+	}
+
+}
---
+++ src/com/timothyaveni/apcsfinal/test/ByteArrayToolsTest.java
@@ -0,0 +1,57 @@
+package com.timothyaveni.apcsfinal.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.timothyaveni.apcsfinal.networking.ByteArrayTools;
+
+public class ByteArrayToolsTest {
+
+	@Test
+	public void testReadBytes() {
+		byte[] data1 = { (byte) 0x00, (byte) 0x00, (byte) 0x00 };
+		assertEquals("0", 0, ByteArrayTools.readBytes(data1, 0, 3));
+
+		byte[] data2 = { (byte) 0xF0, (byte) 0x00, (byte) 0x00 };
+		assertEquals("0xF00000", 15728640, ByteArrayTools.readBytes(data2, 0, 3));
+
+		byte[] data3 = { (byte) 0x45, (byte) 0x01, (byte) 0xD5, (byte) 0xA4, (byte) 0x8B, (byte) 0x00 };
+		assertEquals("failure on length > 5", -1, ByteArrayTools.readBytes(data3, 0, 5));
+
+		assertEquals("failure on length < 1", -1, ByteArrayTools.readBytes(data3, 0, 0));
+		assertEquals("failure on length < 1", -1, ByteArrayTools.readBytes(data3, 0, -1));
+
+		assertEquals("startIndex = 0, length = 4", 1157748132, ByteArrayTools.readBytes(data3, 0, 4));
+		assertEquals("startIndex = 2, length = 2", 54692, ByteArrayTools.readBytes(data3, 2, 2));
+		assertEquals("startIndex = 4, length = 2", 35584, ByteArrayTools.readBytes(data3, 4, 2));
+
+		assertEquals("failure on length outside of bounds", -1, ByteArrayTools.readBytes(data3, 4, 3));
+		assertEquals("failure on startIndex outside of bounds", -1, ByteArrayTools.readBytes(data3, 6, 1));
+	}
+
+	@Test
+	public void testSetBytes() {
+		byte[] data1 = { (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00 };
+		byte[] data1Expected = { (byte) 0x00, (byte) 0xBC, (byte) 0x61, (byte) 0x4E };
+
+		ByteArrayTools.setBytes(data1, 12345678, 0, 4);
+
+		assertArrayEquals("byte 1", data1Expected, data1);
+
+		// you're not actually going to make me test all that boring stuff, are
+		// you?
+
+		byte[] testArray = new byte[4];
+		for (int i = 0; i < 10000000; i++) { // the fun part.
+			int toSet = (int) (Math.random() * Math.pow(256, 4) / 2); // 0..2.147B
+			ByteArrayTools.setBytes(testArray, toSet, 0, 4);
+
+			assertEquals("self-reference test", ByteArrayTools.readBytes(testArray, 0, 4), toSet);
+		}
+
+		// Finishes in about 16 seconds on my pc. This is about 625k runs per
+		// second (1.6 us/run).
+		// Not terrible.
+	}
+}
