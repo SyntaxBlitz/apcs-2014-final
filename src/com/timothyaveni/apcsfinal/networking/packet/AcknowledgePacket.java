12cd9c04362f19414ef9e3f002ad8ea33600e022
Timothy Aveni <timothy@tacosareawesome.com>
2014-05-25 15:28:38 -0400

    Add acknowledgedPacketId to AcknowledgePacket definition

--- src/com/timothyaveni/apcsfinal/networking/packet/AcknowledgePacket.java
+++ src/com/timothyaveni/apcsfinal/networking/packet/AcknowledgePacket.java
@@ -3,6 +3,8 @@ package com.timothyaveni.apcsfinal.networking.packet;
 import com.timothyaveni.apcsfinal.networking.PacketType;
 
 public class AcknowledgePacket extends Packet {
+	
+	private int acknowledgedPacketId;
 
 	public AcknowledgePacket(int id, byte[] data) {
 		super(id);
