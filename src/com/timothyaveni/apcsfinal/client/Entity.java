f467466fb258f9dde6d8068f46cd4af0deaaf775
NFosterJava <whycantifindausername12@gmail.com>
2014-05-27 09:18:00 -0400

    Fixing Dan's stuff

--- src/com/timothyaveni/apcsfinal/client/Entity.java
+++ src/com/timothyaveni/apcsfinal/client/Entity.java
@@ -1,18 +1,23 @@
 package com.timothyaveni.apcsfinal.client;
 
+
 public abstract class Entity {
 
-	public int height; // height of the entity in pixels
-	public int width; // width of the entity in pixels
-	public String name; // name of the entity in terms of type (so Tank's name
-						// is Tank)
-	public String fileLocation; // stream that represents the files location in
-								// the res folder
-	public int strength; // strength attribute determines health
-	public int intelligence; // intelligence attribute determines mana
-	public int speed; // speed determines stamina
 
-	public Entity(String name, String location, int height, int width, int strength, int intelligence, int speed) {
+	private int height; // height of the entity in pixels
+	private int width; // width of the entity in pixels
+	private String name; // name of the entity in terms of type (so Tank's name
+						// is Tank)
+	private String fileLocation; // stream that represents the files location in
+								// the res folder
+	private int strength; // strength attribute determines health
+	private int intelligence; // intelligence attribute determines mana
+	private int speed; // speed determines stamina
+	
+	private Location loc;
+
+
+	public Entity(String name, String location, int height, int width, int strength, int intelligence, int speed, Location loc) {
 		this.name = name;
 		fileLocation = location;
 		this.height = height;
@@ -20,26 +25,34 @@ public abstract class Entity {
 		this.strength = strength;
 		this.intelligence = intelligence;
 		this.speed = speed;
+		this.loc = loc;
 	}
+
 
 	public String getName() {
 		return name;
 	}
 
+
 	public int getHeight() {
 		return height;
 	}
+
 
 	public int getWidth() {
 		return width;
 	}
 
+
 	public String getFileLocation() {
 		return fileLocation;
 	}
 
+
 	public abstract int attack();
 
-	public abstract Location move();
+
+	public abstract void move();
+
 
 }
