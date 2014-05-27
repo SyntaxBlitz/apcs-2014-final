25b3410e3252cb8302b6792d9b45962e71484850
fisherd <danjsurf@gmail.com>
2014-05-27 09:25:46 -0400

    stuff

---
+++ src/com/timothyaveni/apcsfinal/client/Character.java
@@ -0,0 +1,19 @@
+package com.timothyaveni.apcsfinal.client;
+
+public abstract class Character extends Entity {
+	
+	
+	public Character(String name, String location, int height, int width, int strength, int intelligence, int speed, Location loc){
+		super(name, location, height, width, strength,  intelligence, speed, loc);
+	}
+
+	@Override
+	public abstract void move();
+	
+	public abstract void attack();
+	
+	
+	
+	
+
+}
