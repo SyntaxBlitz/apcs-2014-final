dae69e175a80f2e684555c2e940e916caabdb33b
Timothy Aveni <timothy@tacosareawesome.com>
2014-05-31 09:31:12 -0400

    Create sender constructor for EntityDamagePacket

--- src/com/timothyaveni/apcsfinal/networking/packet/EntityDamagePacket.java
+++ src/com/timothyaveni/apcsfinal/networking/packet/EntityDamagePacket.java
@@ -4,8 +4,25 @@ import com.timothyaveni.apcsfinal.networking.PacketType;
 
 public class EntityDamagePacket extends Packet {
 
+	private int entityId;
+	private int damageAmount;
+
 	public EntityDamagePacket(int id, byte[] data) {
 		super(id);
+	}
+
+	public EntityDamagePacket(int id, int entityId, int damageAmount) {
+		super(id);
+		this.entityId = entityId;
+		this.damageAmount = damageAmount;
+	}
+
+	public int getEntityId() {
+		return this.entityId;
+	}
+
+	public int getDamageAmount() {
+		return this.damageAmount;
 	}
 
 	@Override
