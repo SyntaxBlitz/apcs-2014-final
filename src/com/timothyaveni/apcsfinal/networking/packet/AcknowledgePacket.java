49d2e3bcaa98336e591a542d98aa71e13112f4bf
syntaxblitz <timothy@tacosareawesome.com>
2014-06-02 09:26:09 -0400

    Write method prototypes to pack and unpack data into and from packets

--- src/com/timothyaveni/apcsfinal/networking/packet/AcknowledgePacket.java
+++ src/com/timothyaveni/apcsfinal/networking/packet/AcknowledgePacket.java
@@ -1,5 +1,6 @@
 package com.timothyaveni.apcsfinal.networking.packet;
 
+import com.timothyaveni.apcsfinal.networking.ByteArrayTools;
 import com.timothyaveni.apcsfinal.networking.PacketType;
 
 public class AcknowledgePacket extends Packet {
@@ -8,8 +9,33 @@ public class AcknowledgePacket extends Packet {
 
 	public AcknowledgePacket(int id, byte[] data) {
 		super(id);
+		this.unpack(data);
 	}
 
+	public AcknowledgePacket(int id, int remotePacketId) {
+		super(id);
+		this.acknowledgedPacketId = remotePacketId;
+	}
+	
+	@Override
+	public void pack(byte[] data) {
+		super.pack(data);
+		ByteArrayTools.setBytes(data, acknowledgedPacketId, 6, 3);
+	}
+	
+	@Override
+	public void unpack(byte[] data) {
+		super.unpack(data);
+		this.acknowledgedPacketId = ByteArrayTools.readBytes(data, 6, 3, false);
+	}
+	
+	@Override
+	public byte[] getByteArray() {
+		byte[] toReturn = new byte[8];
+		pack(toReturn);
+		return toReturn;
+	}
+	
 	@Override
 	public PacketType getPacketType() {
 		return PacketType.ACKNOWLEDGE;
--- src/com/timothyaveni/apcsfinal/networking/packet/Packet.java
+++ src/com/timothyaveni/apcsfinal/networking/packet/Packet.java
@@ -1,11 +1,15 @@
 package com.timothyaveni.apcsfinal.networking.packet;
 
+import com.timothyaveni.apcsfinal.networking.ByteArrayTools;
 import com.timothyaveni.apcsfinal.networking.PacketType;
+import com.timothyaveni.apcsfinal.networking.PacketTypeID;
 
 public abstract class Packet {
 
 	private int id;
 	private int remoteId;
+	private int senderId;
+	private boolean mustAcknowledge;
 
 	public Packet(int id) {
 		this.id = id;
@@ -22,6 +26,37 @@ public abstract class Packet {
 	public int getRemoteId() {
 		return this.remoteId;
 	}
+
+	public int getSenderId() {
+		return senderId;
+	}
+
+	public void setSenderId(int senderId) {
+		this.senderId = senderId;
+	}
+
+	public boolean isMustAcknowledge() {
+		return mustAcknowledge;
+	}
+
+	public void setMustAcknowledge(boolean mustAcknowledge) {
+		this.mustAcknowledge = mustAcknowledge;
+	}
+
+	protected void unpack(byte[] data) {
+		this.remoteId = ByteArrayTools.readBytes(data, 1, 3, false);
+		this.senderId = ByteArrayTools.readBytes(data, 4, 1, false);
+		this.mustAcknowledge = (ByteArrayTools.readBytes(data, 5, 1, false) == 1);
+	}
+
+	protected void pack(byte[] data) {
+		ByteArrayTools.setBytes(data, PacketTypeID.getID(this.getPacketType()), 0, 1);
+		ByteArrayTools.setBytes(data, this.id, 1, 3);
+		ByteArrayTools.setBytes(data, this.senderId, 4, 1);
+		ByteArrayTools.setBytes(data, this.mustAcknowledge ? 1 : 0, 5, 1);
+	}
+
+	public abstract byte[] getByteArray();
 
 	public abstract PacketType getPacketType();
 
