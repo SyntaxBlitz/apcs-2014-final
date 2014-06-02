1d5b66e95408eaf054b60f8abb18bbee7633807f
NFosterJava <whycantifindausername12@gmail.com>
2014-06-02 19:19:38 -0400

    Update Character.java
    Added getLocation() method

--- src/com/timothyaveni/apcsfinal/client/Character.java
+++ src/com/timothyaveni/apcsfinal/client/Character.java
@@ -1,14 +1,18 @@
 package com.timothyaveni.apcsfinal.client;
 
 public abstract class Character extends Entity {
-	
-	
+
+
 	public Character(String name, String location, int height, int width, int strength, int intelligence, int speed, Location loc){
 		super(name, location, height, width, strength,  intelligence, speed, loc);
 	}
 
 	public void move(int distance, int direction, String plane){}
 	
+	public Location getLocation(){
+		return this.loc;
+	}
+
 	public void characterMove(boolean[] keyboard){
 		if(keyboard[0]){
 			if(keyboard[2]);
@@ -16,9 +20,9 @@ public abstract class Character extends Entity {
 				loc.setX(loc.getX() + 8);
 		}
 		else if(keyboard[1]){
-			if(keyBoard[3]);
+			if(keyboard[3]);
 			else
-				loc.setY(loc.getY + 8);
+				loc.setY(loc.getY() + 8);
 		}
 		else if(keyboard[2]){
 			if(keyboard[0]);
@@ -31,8 +35,8 @@ public abstract class Character extends Entity {
 				loc.setY(loc.getY() - 8);
 		}
 	}
-	
+
 	public abstract Entity attack();
-	
+
 
 }
