388fab84e3efa72404f86b1f59a7500e273bbe79
d-fisher <Danjsurf@gmail.com>
2014-06-02 09:31:08 -0400

    Update Tank.java

--- src/com/timothyaveni/apcsfinal/client/Tank.java
+++ src/com/timothyaveni/apcsfinal/client/Tank.java
@@ -2,15 +2,19 @@ package com.timothyaveni.apcsfinal.client;
 
 public class Tank extends Character {
 	
-	private final int BASE_DAMAGE; 
+	private final int BASE_DAMAGE = 10;
 	
 	public Tank(String name, String location, int height, int width, int strength, int intelligence, int speed, Location loc){
 		super(name, location, height, width, strength,  intelligence, speed, loc);
 	}
 
 	@Override
-	public void move() {
-		//moves sixteen pixels in four possible directions
+	public void characterMove(boolean[] keyboard) {
+		if(keyboard[0] == true){
+			if(keyboard[1] == true);
+			else
+				loc.setX(loc.getX() + 16);
+		}
 
 	}
 
