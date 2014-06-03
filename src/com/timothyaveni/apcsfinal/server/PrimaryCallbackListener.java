2eee1221485bcdc28e2ae1734987c9da03d9da84
Timothy Aveni <timothy@tacosareawesome.com>
2014-06-03 16:04:38 -0400

    Add PrimaryCallbackListener (never got committed, but is referred to in other code)

---
+++ src/com/timothyaveni/apcsfinal/server/PrimaryCallbackListener.java
@@ -0,0 +1,52 @@
+package com.timothyaveni.apcsfinal.server;
+
+import com.timothyaveni.apcsfinal.networking.packet.AcknowledgePacket;
+import com.timothyaveni.apcsfinal.networking.packet.EntityDamagePacket;
+import com.timothyaveni.apcsfinal.networking.packet.PlayerLocationPacket;
+import com.timothyaveni.apcsfinal.networking.server.ServerCallbackListener;
+
+public class PrimaryCallbackListener extends ServerCallbackListener {
+
+	private Server server;
+	
+	public PrimaryCallbackListener(Server server) {
+		this.server = server;
+	}
+	
+	@Override
+	public void bindSuccess() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void bindFail() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void receiveFailure() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void playerMoved(PlayerLocationPacket p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void entityDamaged(EntityDamagePacket packet) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void packetAcknowledged(AcknowledgePacket packet) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
