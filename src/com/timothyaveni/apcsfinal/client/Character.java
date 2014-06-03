bdde7a3fdc0db1ce23482f07045a181f0d3f2108
d-fisher <Danjsurf@gmail.com>
2014-06-03 09:29:09 -0400

    Update Character.java

--- src/com/timothyaveni/apcsfinal/client/Character.java
+++ src/com/timothyaveni/apcsfinal/client/Character.java
@@ -1,13 +1,21 @@
 package com.timothyaveni.apcsfinal.client;
 
-public abstract class Character extends Entity {
+public abstract class Player extends Entity {
+	private int velocity;
+	private int baseDamage;
 
-
-	public Character(String name, String location, int height, int width, int strength, int intelligence, int speed, Location loc){
+	public Player(String name, String location, int height, int width, int strength, int intelligence, int speed, Location loc){
 		super(name, location, height, width, strength,  intelligence, speed, loc);
 	}
 
-	public void move(int distance, int direction, String plane){}
+	public void setVelocity(int s){
+		velocity = s;
+	}
+	
+	public void setBaseDamage(int d){
+		baseDamage = d;
+	}
+	
 	
 	public Location getLocation(){
 		return this.loc;
@@ -16,23 +24,26 @@ public abstract class Character extends Entity {
 	public void characterMove(boolean[] keyboard){
 		if(keyboard[0]){
 			if(keyboard[2]);
-			else
-				loc.setX(loc.getX() + 8);
+			else{
+				loc.setX(loc.getX() + velocity);
+				loc.setDirection(3);
+			}
 		}
 		else if(keyboard[1]){
 			if(keyboard[3]);
 			else
-				loc.setY(loc.getY() + 8);
+				loc.setY(loc.getY() + velocity);
+				
 		}
 		else if(keyboard[2]){
 			if(keyboard[0]);
 			else
-				loc.setX(loc.getX() - 8);
+				loc.setX(loc.getX() - velocity);
 		}
 		else if(keyboard[3]){
 			if(keyboard[1]);
 			else
-				loc.setY(loc.getY() - 8);
+				loc.setY(loc.getY() - velocity);
 		}
 	}
 
