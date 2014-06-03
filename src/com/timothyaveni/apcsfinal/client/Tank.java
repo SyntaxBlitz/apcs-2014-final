5deafdfb9806aa4f75675a100a9d67e64c4eb9ef
d-fisher <Danjsurf@gmail.com>
2014-06-03 09:30:15 -0400

    Update Tank.java

--- src/com/timothyaveni/apcsfinal/client/Tank.java
+++ src/com/timothyaveni/apcsfinal/client/Tank.java
@@ -1,11 +1,12 @@
 package com.timothyaveni.apcsfinal.client;
 
-public class Tank extends Character {
+public class Tank extends Player {
 	
-	private final int BASE_DAMAGE = 10;
 	
 	public Tank(String name, String location, int height, int width, int strength, int intelligence, int speed, Location loc){
 		super(name, location, height, width, strength,  intelligence, speed, loc);
+		setVelocity(8);
+		setBaseDamage(12);
 	}
 
 	
