7267db0523f37b9063da4d3c7a947e1565714382
Timothy Aveni <timothy@tacosareawesome.com>
2014-06-03 17:32:58 -0400

    actually add FileReader class

---
+++ src/com/timothyaveni/apcsfinal/client/FileReader.java
@@ -0,0 +1,11 @@
+package com.timothyaveni.apcsfinal.client;
+
+import java.io.File;
+
+public class FileReader {
+
+	public static File getFileFromResourceString(String path) {
+		return new File("res/" + path);
+	}
+	
+}
