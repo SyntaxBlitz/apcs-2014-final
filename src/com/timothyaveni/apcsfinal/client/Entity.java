26db2f6075de82004b9b07e5c9a23617db508885
Timothy Aveni <timothy@tacosareawesome.com>
2014-06-03 16:11:48 -0400

    Fix up Entity class

--- src/com/timothyaveni/apcsfinal/client/Entity.java
+++ src/com/timothyaveni/apcsfinal/client/Entity.java
@@ -1,4 +1,4 @@
-ppackage com.timothyaveni.apcsfinal.client;
+package com.timothyaveni.apcsfinal.client;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -8,67 +8,46 @@ import javax.imageio.ImageIO;
 
 public abstract class Entity {
 
-	protected int height; // height of the entity in pixels
-	protected int width; // width of the entity in pixels
-	protected String name; // name of the entity in terms of type (so Tank's
-							// name
-							// is Tank)
-	protected String fileLocation; // stream that represents the files location
-									// in
-									// the res folder
-	protected int strength; // strength attribute determines health
-	protected int intelligence; // intelligence attribute determines mana
-	protected int speed; // speed determines stamina
-
+	protected int id;
 	protected Location loc;
 
 	protected boolean inCombat;
 
-	public Entity(String name, String location, int height, int width,
-			int strength, int intelligence, int speed, Location loc) {
-		this.name = name;
-		fileLocation = location;
-		this.height = height;
-		this.width = width;
-		this.strength = strength;
-		this.intelligence = intelligence;
-		this.speed = speed;
+	public Entity(int id, Location loc) {
+		this.id = id;
 		this.loc = loc;
 	}
 
-	public String getName() {
-		return name;
+	public abstract int getHeight();
+
+	public abstract int getWidth();
+
+	public abstract String getFileLocation();
+
+	public abstract int getStrength();
+
+	public abstract int getSpeed();
+
+	public abstract int getIntelligence();
+
+	public Location getLocation() {
+		return this.loc;
 	}
 
-	public int getHeight() {
-		return height;
-	}
-
-	public int getWidth() {
-		return width;
-	}
-
-	public String getFileLocation() {
-		return fileLocation;
-	}
-
-
-	public BufferedImage getImage(Location loc) {
+	public BufferedImage getImage(long frame) {
 		BufferedImage image = null;
 		if (inCombat == false) {
 			try {
-				image = ImageIO.read(new File(fileLocation));
-				image = image.getSubimage((loc.getSteps() % 4) * 32,
-						loc.getDirection() * 48, this.getWidth(),
+				image = ImageIO.read(new File(getFileLocation()));
+				image = image.getSubimage((int) ((frame % 4) * 32), loc.getDirection() * 48, this.getWidth(),
 						this.getHeight());
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		} else {
 			try {
-				image = ImageIO.read(new File(fileLocation));
-				image = image.getSubimage((loc.getSteps() % 2) * 32 + 128,
-						loc.getDirection() * 48, this.getWidth(),
+				image = ImageIO.read(new File(getFileLocation()));
+				image = image.getSubimage((int) ((frame % 2) * 32 + 128), loc.getDirection() * 48, this.getWidth(),
 						this.getHeight());
 			} catch (IOException e) {
 				e.printStackTrace();
