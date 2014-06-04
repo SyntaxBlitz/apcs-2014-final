27354110e7c04155f16c79b4f4f542df44b2a34d
CMM1313 <c.m.muller@hotmail.com>
2014-06-04 15:54:09 -0400

    Insert buffer starategy

--- src/com/timothyaveni/apcsfinal/client/gui/MapCanvas.java
+++ src/com/timothyaveni/apcsfinal/client/gui/MapCanvas.java
@@ -3,6 +3,7 @@ package com.timothyaveni.apcsfinal.client.gui;
 import java.awt.Canvas;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.util.List;
 
@@ -18,41 +19,44 @@ public class MapCanvas extends Canvas implements UsesClient {
 	private Client client;
 
 	private Map map;
+	private BufferStrategy strategy;
 
 	// Constructor :D
 	public MapCanvas() {
 		super();
 		map = new Map("Map1.png");
+		this.createBufferStrategy(2);
+		strategy = this.getBufferStrategy();
 	}
 
-	public void render(Graphics g) {
-		BufferedImage buffer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-		Graphics2D bufferGraphics = buffer.createGraphics();
+	public void render() {
+		do {
+			do {
+				Graphics g = strategy.getDrawGraphics();
+				BufferedImage buffer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+				Graphics2D bufferGraphics = buffer.createGraphics();
 
-		Location playerLocation = client.getPlayer().getLocation();
-		bufferGraphics.drawImage(map.getPic(playerLocation), 0, 0, 1024, 768, null);
+				Location playerLocation = client.getPlayer().getLocation();
+				bufferGraphics.drawImage(map.getPic(playerLocation), 0, 0, 1024, 768, null);
 
-		List<Entity> entities = client.getEntityList();
+				List<Entity> entities = client.getEntityList();
 
-		for (int i = 0; i < entities.size(); i++) {
-			Entity thisEntity = entities.get(i);
-			bufferGraphics.drawImage(thisEntity.getImage(client.getFrame()), thisEntity.getLocation().getX()
-					- playerLocation.getX() + WIDTH / 2 - thisEntity.getWidth() / 2, thisEntity.getLocation().getY()
-					- playerLocation.getY() + HEIGHT / 2 - thisEntity.getHeight() / 2, thisEntity.getWidth(),
-					thisEntity.getHeight(), null);
-		}
+				for (int i = 0; i < entities.size(); i++) {
+					Entity thisEntity = entities.get(i);
+					bufferGraphics.drawImage(thisEntity.getImage(client.getFrame()), thisEntity.getLocation().getX()
+							- playerLocation.getX() + WIDTH / 2 - thisEntity.getWidth() / 2, thisEntity.getLocation()
+							.getY() - playerLocation.getY() + HEIGHT / 2 - thisEntity.getHeight() / 2,
+							thisEntity.getWidth(), thisEntity.getHeight(), null);
+				}
 
-		// pull arraylist down off client
-		// misc comment so I'm able to commit
-		// g.dispose(); // releases system resources the graphics object is
-		// taking
-		// up
-		g.drawImage(buffer, 0, 0, null);
-	}
+				g.drawImage(buffer, 0, 0, null);
 
-	@Override
-	public void paint(Graphics g) {
-		render(g);
+				// pull arraylist down off client
+				// misc comment so I'm able to commit
+				g.dispose(); // releases system resources the graphics object is
+			} while (strategy.contentsRestored());
+			strategy.show();
+		} while (strategy.contentsLost());
 	}
 
 	@Override
