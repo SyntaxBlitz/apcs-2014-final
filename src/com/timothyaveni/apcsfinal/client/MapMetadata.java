b596a538b36c287f6431242be4570b06e846f52e
Timothy Aveni <timothy@tacosareawesome.com>
2014-06-07 22:35:06 -0400

    Finish exit area detection code

--- src/com/timothyaveni/apcsfinal/client/MapMetadata.java
+++ src/com/timothyaveni/apcsfinal/client/MapMetadata.java
@@ -1,5 +1,6 @@
 package com.timothyaveni.apcsfinal.client;
 
+import java.awt.Rectangle;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -15,8 +16,14 @@ import com.timothyaveni.apcsfinal.networking.EntityTypeID;
 
 public class MapMetadata {
 
+	private class ExitArea {
+		Rectangle rectangle;
+		int nextMap;
+	}
+
 	private ArrayList<EntityInfo> loadedEntityInfo;
 	private Location spawnPoint;
+	private ArrayList<ExitArea> exitAreas;
 
 	public MapMetadata(File file, Map map) {
 		JSONObject jsonObject = null;
@@ -37,12 +44,23 @@ public class MapMetadata {
 			JSONObject entityObject = entityArray.getJSONObject(i);
 			JSONObject locationObject = entityObject.getJSONObject("location");
 			loadedEntityInfo.add(new EntityInfo(EntityTypeID.getType(entityObject.getInt("type")), new Location(
-					locationObject.getInt("x"), locationObject.getInt("y"), locationObject.getInt("direction"), map.getWorldSectionId())));
+					locationObject.getInt("x"), locationObject.getInt("y"), locationObject.getInt("direction"), map
+							.getWorldSectionId())));
 		}
 
 		JSONObject spawnPointLocationObject = jsonObject.getJSONObject("spawnPoint");
 		this.spawnPoint = new Location(spawnPointLocationObject.getInt("x"), spawnPointLocationObject.getInt("y"),
 				spawnPointLocationObject.getInt("direction"), map.getWorldSectionId());
+
+		JSONArray exitAreaArray = jsonObject.getJSONArray("exitAreas");
+		for (int i = 0; i < exitAreaArray.length(); i++) {
+			JSONObject exitAreaObject = exitAreaArray.getJSONObject(i);
+			ExitArea area = new ExitArea();
+			area.rectangle = new Rectangle(exitAreaObject.getInt("x"), exitAreaObject.getInt("y"),
+					exitAreaObject.getInt("width"), exitAreaObject.getInt("height"));
+			area.nextMap = exitAreaObject.getInt("newMapId");
+			exitAreas.add(area);
+		}
 	}
 
 	public List<EntityInfo> getEntityInfo() {
@@ -51,10 +69,14 @@ public class MapMetadata {
 
 	/**
 	 * 
-	 * @return null if the point is not in any exit zone, else the new map name
+	 * @return -1 if the point is not in any exit zone, else the new map world section id
 	 */
-	public String getNextMap() {
-		return null;
+	public int getNextMap(int x, int y) {
+		for (ExitArea area: exitAreas) {
+			if (area.rectangle.contains(x, y))
+				return area.nextMap;
+		}
+		return -1;
 	}
 
 	public Location getSpawnPoint() {
