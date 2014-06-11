e1d98718f75bc4d4afe0645b3afadfbd46a81551
Timothy Aveni <timothy@tacosareawesome.com>
2014-06-10 21:02:42 -0400

    Add sparks for mage

--- src/com/timothyaveni/apcsfinal/client/environmentanimation/LightningBolt.java
+++ src/com/timothyaveni/apcsfinal/client/environmentanimation/LightningBolt.java
@@ -1,25 +1,43 @@
 package com.timothyaveni.apcsfinal.client.environmentanimation;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+import com.timothyaveni.apcsfinal.client.FileReader;
 import com.timothyaveni.apcsfinal.client.Location;
 
 public class LightningBolt extends EnvironmentAnimation {
 
+	BufferedImage image;
+	
 	public LightningBolt(long startFrame, Location location) {
 		super(startFrame, location);
+		try {
+			image = ImageIO.read(FileReader.getFileFromResourceString("MagicSparks.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public BufferedImage getImage(long frame) {
-		// TODO Auto-generated method stub
-		return null;
+		int renderFrame = (int) ((frame - getStartFrame()) / 5);
+		return image.getSubimage(renderFrame * getWidth(), 0, getWidth(), getHeight());
 	}
 
+	private int getWidth() {
+		return 288;
+	}
+	
+	private int getHeight() {
+		return 288;
+	}
+	
 	@Override
 	public boolean stillRelevant(long frame) {
-		// TODO Auto-generated method stub
-		return false;
+		return frame - getStartFrame() < 25;
 	}
 
 }
